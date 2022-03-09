f8f4b3d768568cfc073be486bae0e67ed9f277d8
Andrean <88448470+QkoSad@users.noreply.github.com>
2022-03-09 20:26:59 +0200

    Add files via upload

---
+++ test/index.ts
@@ -0,0 +1,10 @@
+enum LoadingState {
+  beforeLoad = "beforeLoad",
+  loading = "loading",
+  loaded = "loaded",
+};
+const enlighLoadingStte = {
+    [LoadingState.beforeLoad]: "before load"
+};
+
+console.log(enlighLoadingStte);
